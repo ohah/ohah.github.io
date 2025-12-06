5b511727ea997075664684a6000b21d56bf4dc37
ohah <bookyoon173@gmail.com>
2025-12-06 19:07:30 +0900

    chore: update dependencies and config

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,11 +1,13 @@
 import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
+import pluginMermaid from 'rspress-plugin-mermaid';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   outDir: path.join(__dirname, 'dist'),
   title: 'ohah',
   globalStyles: path.join(__dirname, 'docs/global.css'),
+  plugins: [pluginMermaid() as any],
   themeConfig: {
     socialLinks: [
       {
